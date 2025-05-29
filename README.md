e5d1630ae75e11fb6919855d1f4c659031c04b48
maheshpachpande <pachpandemahesh300@gmail.com>
2025-05-29 10:04:45 +0530

    mlFlow added

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-echo "# Student_Maths" >> README.md
+<!-- echo "# Student_Maths" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/maheshpachpande/Student_Maths.git
-git push -u origin main
+git push -u origin main -->
 
 
-git remote add origin https://github.com/maheshpachpande/Student_Maths.git
-git branch -M main
-git push -u origin main
+->git remote add origin https://github.com/maheshpachpande/Student_Maths.git
+->git branch -M main
+->git push -u origin main
 
 conda create -p student_venv python=3.8 -y
 
@@ -18,3 +18,12 @@ conda activate C:\Users\Mahesh\Desktop\student_maths\student_venv
 or
 
 conda activate student_venv/
+
+
+import dagshub
+dagshub.init(repo_owner='pachpandemahesh300', repo_name='mlproject_student', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- src/mlproject/components/model_tranier.py
+++ src/mlproject/components/model_tranier.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import dagshub
 from dataclasses import dataclass
 from urllib.parse import urlparse
 import mlflow
@@ -117,35 +118,39 @@ class ModelTrainer:
 
             best_params = params[actual_model]
             print(best_params)
+            
+           
+            logging.info(f"Best model found: {best_model_name} with score: {best_model_score}")
+            # mlflow tracking URI
+            mlflow.set_registry_uri("https://dagshub.com/pachpandemahesh300/mlproject_student.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-            # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/mlprojecthindi.mlflow")
-            # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            # mlflow
+            dagshub.init(repo_owner='pachpandemahesh300', repo_name='mlproject_student', mlflow=True)
 
-            # # mlflow
+            with mlflow.start_run():
+                # best model prediction
+                predicted_qualities = best_model.predict(X_test)
 
-            # with mlflow.start_run():
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
 
-            #     predicted_qualities = best_model.predict(X_test)
+                mlflow.log_params(best_params)
 
-            #     (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
-
-            #     mlflow.log_params(best_params)
-
-            #     mlflow.log_metric("rmse", rmse)
-            #     mlflow.log_metric("r2", r2)
-            #     mlflow.log_metric("mae", mae)
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
 
 
-            #     # Model registry does not work with file store
-            #     if tracking_url_type_store != "file":
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
 
-            #         # Register the model
-            #         # There are other ways to use the Model Registry, which depends on the use case,
-            #         # please refer to the doc for more information:
-            #         # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-            #         mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
-            #     else:
-            #         mlflow.sklearn.log_model(best_model, "model")
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
 
 
 
